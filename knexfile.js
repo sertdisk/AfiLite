43e2b6e8f08909257468fc2aba2374eba6e615cc
sertdisk <sertdisk@gmail.com>
2025-08-02 04:04:36 +0300

    Kinex tamam veri yabanında demo kod ve satış komisyonları tamam

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,16 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    connection: { filename: './data/afilite.db' },
+    useNullAsDefault: true,
+    migrations: { directory: './src/db/migrations' },
+    seeds: { directory: './src/db/seeds' }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'sqlite3',
+    connection: { filename: process.env.DB_PATH || './data/afilite.db' },
+    useNullAsDefault: true,
+    migrations: { directory: './src/db/migrations' },
+    seeds: { directory: './src/db/seeds' }
   }
-
 };
